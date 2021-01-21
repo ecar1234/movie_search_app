4ad37f1fdc0e1d62acb09ea32143498819fd5b36
jk <ecar1234@gmail.com>
2021-01-21 21:46:15 +0900

    Movie app setting completed(Proceeding)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
+import Movie from './Movie'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const movies = 
+    await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+    this.setState({
+      movies: movies.data.data.movies, isLoading: false
+    })
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader_text">
+            Loading...
+          </span>
+        </div> 
+        ) : (
+        <div>
+          <div className="search_input_wrap">
+            <span>영화 검색 : </span>
+            <input className="search_input"/>
+            <button className="search_button" autoFocus>Search</button>
+          </div>
+          <div className="movies">
+            {movies.map(movie => (
+            <Movie 
+              key={movie.id} 
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              genres={movie.genres} 
+            />
+          ))}
+          </div>
+        </div>
+        )}
+      </section>
+    );
+   }
 }
 
 export default App;
